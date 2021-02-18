59e06b43a95d846443417e0691c8ead54c988214
mygit20031984 <ashuklkrn@gmail.com>
2021-02-18 12:49:07 +0530

    Updates.

--- ML_housing.py
+++ ML_housing.py
@@ -3,6 +3,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+
 melbourne_file_path = 'input/melbourne-housing-snapshot/melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
 melbourne_data = melbourne_data.dropna(axis=0)
@@ -20,10 +21,15 @@ melbourne_model = DecisionTreeRegressor()
 melbourne_model.fit(train_X, train_y)
 
 
-val_predictions = melbourne_model.predict(val_X)
-
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
-melbourne_model = DecisionTreeRegressor()
-melbourne_model.fit(train_X, train_y)
-val_predictions = melbourne_model.predict(val_X)
-print(mean_absolute_error(val_y, val_predictions))
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t\t   Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
