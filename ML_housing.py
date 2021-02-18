f99a6a914015403d5782de5109d3849bb52e9dd5
mygit20031984 <ashuklkrn@gmail.com>
2021-02-18 11:07:21 +0530

    Initial commit

---
+++ ML_housing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+melbourne_file_path = 'input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path)
+melbourne_data = melbourne_data.dropna(axis=0)
+
+y = melbourne_data.Price
+
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
