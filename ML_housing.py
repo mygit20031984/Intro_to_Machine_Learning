12a97fe9862e0c8c9a65533256b645afd67f5dc4
mygit20031984 <ashuklkrn@gmail.com>
2021-02-18 12:13:34 +0530

    Updates.

--- ML_housing.py
+++ ML_housing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 melbourne_file_path = 'input/melbourne-housing-snapshot/melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -7,13 +9,21 @@ melbourne_data = melbourne_data.dropna(axis=0)
 
 y = melbourne_data.Price
 
-melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                        'YearBuilt', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]
 
 melbourne_model = DecisionTreeRegressor(random_state=1)
-melbourne_model.fit(X, y)
 
-print("Making predictions for the following 5 houses:")
-print(X.head())
-print("The predictions are")
-print(melbourne_model.predict(X.head()))
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+melbourne_model = DecisionTreeRegressor()
+melbourne_model.fit(train_X, train_y)
+
+
+val_predictions = melbourne_model.predict(val_X)
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+melbourne_model = DecisionTreeRegressor()
+melbourne_model.fit(train_X, train_y)
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
